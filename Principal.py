f5a498a3f36d3a6f18bd0c8c8aa7012e496eb714
Juelis Jr <juelis.jr@outlook.com>
2013-07-09 14:20:36 -0300

    Aplicado PEP 8 no arquivo

--- Principal.py
+++ Principal.py
@@ -3,12 +3,12 @@
 from Diamond import Diamond
 
 d = Diamond()
-d.printDiamond('e')
+d.printDiamond('E')
 
 """
-	Em python os metodos e classe podem ser tratados tambem como estaticos
-	(nao ha obrigatoriedade de se ter uma instancia do objeto para ter acesso aos atributos metodos)
+Em python os metodos e classe podem ser tratados tambem como estaticos
+(nao ha obrigatoriedade de se ter uma instancia do objeto para ter acesso aos atributos metodos)
 
-	A proxima instrucao tera o mesmo efeito
+A proxima instrucao tera o mesmo efeito
 """
 #Diamond().printDiamond('w')
