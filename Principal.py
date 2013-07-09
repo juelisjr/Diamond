87ee80a6130d9e70c8d5f0f48e6482a173480d44
Juelis Jr <juelis.jr@outlook.com>
2013-07-09 14:36:51 -0300

    Usuario digita qual letra deseja para visualizar o Diamante

--- Principal.py
+++ Principal.py
@@ -2,8 +2,10 @@
 #-*-encoding: utf-8 -*-
 from Diamond import Diamond
 
+letra = raw_input("Digite uma letra para fazer a impressao do Diamante: ")
+
 d = Diamond()
-d.printDiamond('E')
+d.printDiamond(letra)
 
 """
 Em python os metodos e classe podem ser tratados tambem como estaticos
